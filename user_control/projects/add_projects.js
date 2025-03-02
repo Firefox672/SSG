c33807c8497e2f5396248c172e9aaee3a45bf480
Firefox672 <mozillafirefox672@gmail.com>
2025-03-02 23:29:16 +0530

    Update add_projects.js

--- user_control/projects/add_projects.js
+++ user_control/projects/add_projects.js
@@ -21,7 +21,7 @@ document.getElementById("youtubeForm").addEventListener("submit", async (e) => {
     const data = { youtubeLink, row };
 
     try {
-        const response = await fetch("http://localhost:10000/submit", { 
+        const response = await fetch("https://ssg-f83i.onrender.com", { 
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data),
