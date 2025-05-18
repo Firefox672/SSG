52964b0f7fa7ff0f1e488402fb17180d1a5f0908
Firefox672 <153491530+Firefox672@users.noreply.github.com>
2025-05-18 10:19:27 +0530

    Update add_projects.js

--- user_control/projects/add_projects.js
+++ user_control/projects/add_projects.js
@@ -21,7 +21,7 @@ document.getElementById("youtubeForm").addEventListener("submit", async (e) => {
     const data = { youtubeLink, row };
 
     try {
-        const response = await fetch("http://localhost:10000/submit", { 
+        const response = await fetch("https://ssg-event-server.onrender.com/submit", { 
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data),
